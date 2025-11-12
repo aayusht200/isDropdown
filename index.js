78741fa5fc4aa27aedf28ffe8dc3b4a4d5b50257
Aayush Trivedi <aayush.t200@gmail.com>
2025-11-12 20:57:04 +0530

    Implement dropdown toggle functionality

---
+++ index.js
@@ -0,0 +1,20 @@
+// [data-dropdown-button] → button that triggers dropdown
+// [data-dropdown] → wrapper of dropdown content
+
+export function isDropdown() {
+    document.addEventListener('click', (e) => {
+        const isDropdown = e.target.matches('[data-dropdown-button]');
+        if (!isDropdown && e.target.closest('[data-dropdown]') !== null) return;
+
+        let currentDropdown;
+        if (isDropdown) {
+            currentDropdown = e.target.closest('[data-dropdown]');
+            currentDropdown.classList.toggle('active');
+        }
+
+        document.querySelectorAll('[data-dropdown].active').forEach((dropdown) => {
+            if (dropdown === currentDropdown) return;
+            dropdown.classList.remove('active');
+        });
+    });
+}
